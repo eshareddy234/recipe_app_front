0a4c90f46c5e8e7f5828c3a4b7a29d214e4c723a
eshareddy234 <119891063+eshareddy234@users.noreply.github.com>
2023-08-10 17:05:27 +0530

    Add files via upload

---
+++ src/pages/saved-recipes.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+
+const baseUrl = process.env.baseUrl || "http://localhost:3001";
+export const SavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `${baseUrl}/recipes/savedRecipes/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchSavedRecipes();
+}, []);
+  return (
+    <div>
+      <h1>Saved Recipes</h1>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <h3>{recipe.description}</h3>
+            </div>
+            <p>{recipe.instructions}</p>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
